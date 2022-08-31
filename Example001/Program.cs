201ad43fb5aab4763b0fdbbf592de78b4b3d3a9b
Ruslan Karimov <ru-ka-@mail.ru>
2022-08-31 21:49:48 +0500

    Нашли решение для поиска максимума из 9 чисел с помощью функции

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,24 +1,26 @@
 ﻿// Поиск максимального из 9 чисел
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
 int a1 = 22;
 int b1 = 50;
 int c1 = 13;
 int a2 = 44;
 int b2 = 86;
-int c2 = 998;
+int c2 = 668;
 int a3 = 64;
 int b3 = 102;
 int c3 = 14;
 
-int max = a1;
-if (b1 > max) max = b1;
-if (c1 > max) max = c1;
-
-if (a2 > max) max = a2;
-if (b2 > max) max = b2;
-if (c2 > max) max = c2;
-
-if (a3 > max) max = a3;
-if (b3 > max) max = b3;
-if (c3 > max) max = c3;
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
 
 Console.WriteLine(max);
