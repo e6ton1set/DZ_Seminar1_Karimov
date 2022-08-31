59aac5cf5edf30fe8ee1049e6242acaf496651bb
Ruslan Karimov <ru-ka-@mail.ru>
2022-08-31 21:28:36 +0500

    Нашли решение с поиском максимума из 9 числе с помощью классического подхода

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Поиск максимального из 9 чисел
+int a1 = 22;
+int b1 = 50;
+int c1 = 13;
+int a2 = 44;
+int b2 = 86;
+int c2 = 998;
+int a3 = 64;
+int b3 = 102;
+int c3 = 14;
+
+int max = a1;
+if (b1 > max) max = b1;
+if (c1 > max) max = c1;
+
+if (a2 > max) max = a2;
+if (b2 > max) max = b2;
+if (c2 > max) max = c2;
+
+if (a3 > max) max = a3;
+if (b3 > max) max = b3;
+if (c3 > max) max = c3;
+
+Console.WriteLine(max);
